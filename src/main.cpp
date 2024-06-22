ac52ff91355c22d5473785cd44f4e12bdde64f69
winterver <295107659@qq.com>
2024-06-22 21:39:54 +0800

    add camera

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,92 @@ struct ColoredVertex
     }
 };
 
+class Camera
+{
+private:
+    float fov = 60.0f;
+    float vertical = 0.0f;
+    float horizontal = glm::pi<float>();
+    float speed = 3.0f;
+    float mouseSpeed = 0.005f;
+    glm::vec3 position = glm::vec3(0.0f, 0.0f, 5.0f);
+
+public:
+    glm::mat4 update(GLFWwindow* window)
+    {
+        static double lastTime = glfwGetTime();
+        double currentTime = glfwGetTime();
+        float deltaTime = float(currentTime - lastTime);
+        lastTime = currentTime;
+
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+        glfwSetCursorPos(window, width/2.0f, height/2.0f);
+
+        horizontal += float(mouseSpeed * (width/2.0f - xpos));
+        vertical   -= float(mouseSpeed * (height/2.0f - ypos));
+        horizontal = glm::mod(horizontal, glm::two_pi<float>());
+        vertical = glm::clamp(vertical, -glm::half_pi<float>(), glm::half_pi<float>());
+
+        glm::vec3 direction(
+            cos(vertical) * sin(horizontal), 
+            sin(vertical),
+            cos(vertical) * cos(horizontal)
+        );
+
+        glm::vec3 forward(
+            sin(horizontal), 
+            0,
+            cos(horizontal)
+        );
+        
+        glm::vec3 right(
+            sin(horizontal - glm::half_pi<float>()), 
+            0,
+            cos(horizontal - glm::half_pi<float>())
+        );
+
+        glm::vec3 up = glm::vec3(0, -1, 0);
+
+        glm::vec3 velocity{};
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
+            velocity += forward;
+            velocity = glm::normalize(velocity);
+        }
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+            velocity -= forward;
+            velocity = glm::normalize(velocity);
+        }
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+            velocity += right;
+            velocity = glm::normalize(velocity);
+        }
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+            velocity -= right;
+            velocity = glm::normalize(velocity);
+        }
+        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS){
+            velocity += up;
+            velocity = glm::normalize(velocity);
+        }
+        if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS){
+            velocity -= up;
+            velocity = glm::normalize(velocity);
+        }
+        position += velocity * speed * deltaTime;
+
+        int fbWdith, fbHeight;
+        glfwGetFramebufferSize(window, &fbWdith, &fbHeight);
+        glm::mat4 projection = glm::perspective(glm::radians(fov), (float)fbWdith/fbHeight, 0.1f, 100.0f);
+        glm::mat4 view       = glm::lookAt(position, position + direction, glm::vec3(0, 1, 0));
+
+        return projection * view;
+    }
+};
+
 class Application
 {
     Application(const Application&) = delete;
@@ -809,6 +895,8 @@ public:
         createFramebuffers();
     }
 
+    Camera camera;
+
     void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex)
     {
         VkCommandBufferBeginInfo beginInfo{};
@@ -846,7 +934,8 @@ public:
             scissor.extent = swapchainExtent;
             vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
 
-            glm::mat4 MVP = glm::rotate((float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+            glm::mat4 model = glm::rotate((float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+            glm::mat4 MVP = camera.update(window) * model;
             vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(glm::mat4), &MVP[0][0]);
 
             VkBuffer vertexBuffers[] = { vertexBuffer };
@@ -867,7 +956,12 @@ public:
     void run()
     {
         glfwShowWindow(window);
-        while (!glfwWindowShouldClose(window)) {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+        glfwSetCursorPos(window, width/2.0f, height/2.0f);
+
+        while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(window)) {
             glfwPollEvents();
 
             vkWaitForFences(device, 1, &inFlightFence, VK_TRUE, UINT64_MAX);
